6eee157a293e9240eabf626851fda3b9168a7f1c
Jeremy Mansfield <jmansfield@brandaiddesignco.com>
2015-04-21 13:12:36 -0500

    Tidy'ed up the Javascript & other updates

--- js/vendor/application.js
+++ js/vendor/application.js
@@ -2,41 +2,39 @@
 // 100% Height Off-Canvas Foundation 5
 // http://stackoverflow.com/questions/20484240/100-height-off-canvas-foundation-5/20941659#20941659
 ////////////////////////////////////////////////
-$(function () {
+$(function() {
     var timer;
-    $(window).resize(function () {
+    $(window).resize(function() {
         clearTimeout(timer);
-        timer = setTimeout(function () {
+        timer = setTimeout(function() {
             $('.inner-wrap').css("min-height", $(window).height() + "px");
         }, 40);
     }).resize();
 });
-
 ////////////////////////////////////////////////
 // back to top
 ////////////////////////////////////////////////
 /* learn how here >> http://brandaid.it/16kSKCu */
 var offset = 220;
 var duration = 500;
-jQuery(window).scroll(function () {
+jQuery(window).scroll(function() {
     if (jQuery(this).scrollTop() > offset) {
         jQuery('#to-top').fadeIn(duration);
     } else {
         jQuery('#to-top').fadeOut(duration);
     }
 });
-jQuery('#to-top').click(function (event) {
+jQuery('#to-top').click(function(event) {
     event.preventDefault();
     jQuery('html, body').animate({
         scrollTop: 0
     }, duration);
     return false;
 });
-
 ////////////////////////////////////////////////
 // hero bxslider
 ////////////////////////////////////////////////
-$(document).ready(function () {
+$(document).ready(function() {
     $('.hero-slider').bxSlider({
         //adaptiveHeight: true,
         mode: 'fade',
@@ -45,11 +43,10 @@ $(document).ready(function () {
         adaptiveHeight: true,
     });
 });
-
 ////////////////////////////////////////////////
 // testimonial bxslider
 ////////////////////////////////////////////////
-$(document).ready(function () {
+$(document).ready(function() {
     $('.quote-slider').bxSlider({
         //adaptiveHeight: true,
         mode: 'fade',
@@ -57,14 +54,12 @@ $(document).ready(function () {
         pause: 8000,
         adaptiveHeight: true,
     });
-
-////////////////////////////////////////////////
-// testimonial bxslider
-////////////////////////////////////////////////
+    ////////////////////////////////////////////////
+    // testimonial bxslider
+    ////////////////////////////////////////////////
     //Set vert tabs height initially ( reset on resize below )
     $(window).width() > 640 ? $('#vertTabs').height($('#vertContent').height()) : '';
 });
-
-$(window).resize(function () {
+$(window).resize(function() {
     $(window).width() > 640 ? $('#vertTabs').height($('#vertContent').height()) : '';
 });
