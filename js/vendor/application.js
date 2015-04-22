802b8a242c3947de6c11c95ae35755407ca7c734
Jeremy Mansfield <jmansfield@brandaiddesignco.com>
2015-04-22 08:03:24 -0500

    Updates & Additional Pages

--- js/vendor/application.js
+++ js/vendor/application.js
@@ -36,12 +36,12 @@ jQuery('#to-top').click(function (event) {
 //////////////////////////////////////////////////
 // vert tab resize on window resize and tab click
 //////////////////////////////////////////////////
-$(window).resize(function () {
-    $(window).width() > 640 ? $('#vertTabs').height($('#vertContent').height()) : $('#vertTabs').height('100%');
-});
-$('#vertTabs li').click(function () {
-    $(window).width() > 640 ? $('#vertTabs').height($('#vertContent').height()) : $('#vertTabs').height('100%');
-});
+//$(window).resize(function () {
+//    $(window).width() > 640 ? $('#vertTabs').height($('#vertContent').height()) : $('#vertTabs').height('100%');
+//});
+//$('#vertTabs li').click(function () {
+//    $(window).width() > 640 ? $('#vertTabs').height($('#vertContent').height()) : $('#vertTabs').height('100%');
+//});
 
 
 $(document).ready(function () { // BEGIN DOCUMENT READY FUNCTION //
@@ -72,6 +72,6 @@ $(document).ready(function () { // BEGIN DOCUMENT READY FUNCTION //
     // TABS HACK
     ////////////////////////////////////////////////
     //Set vert tabs height initially ( reset on resize below )
-    $(window).width() > 640 ? $('#vertTabs').height($('#vertContent').height()) : $('#vertTabs').height('100%');
+    //$(window).width() > 640 ? $('#vertTabs').height($('#vertContent').height()) : $('#vertTabs').height('100%');
 
 }); // END DOCUMENT READY FUNCTION //
